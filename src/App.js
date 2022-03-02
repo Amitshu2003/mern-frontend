c3a4e119ca41cb3d8bb1dc2d471036ffe6f900cc
Amit Shukla <201951021@iiitvadodara.ac.in>
2022-03-02 21:03:15 +0530

    final code

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
   // const token = localStorage.getItem("token");
   return (
     <div>
-      <h1>Hello </h1>
       <BrowserRouter>
         <Routes>
           <Route path="/login" exact element={<Login />} />          
